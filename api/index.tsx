69e62c29a972d2395623fca16952f80b5a174443
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-18 00:47:33 -0500

    ffbfdxb

--- api/index.tsx
+++ api/index.tsx
@@ -261,7 +261,7 @@ app.frame('/check-allowance', async (c) => {
             textShadow: '2px 2px 4px rgba(0,0,0,0.5)',
           }}
         >
-          <div style={{ display: 'flex' }}>Looks like you don't have a allowance</div>
+          <div style={{ display: 'flex' }}>Error, or you don't have an allowance</div>
         </div>
       ),
       intents: [
